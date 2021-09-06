c0e8586eae6a883543b18439c9dafb63e91fc91b
Soffee <sopee_namyen@hotmail.com>
2021-09-06 17:14:20 +0700

    add images css

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import EventList from "../components/events/EventList";
+import { getFeaturedEvents } from "../dummy-data";
 
 const HomePage = () => {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>home page</h1>
+      <EventList items={featuredEvents}/>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
