717d81b29e352f12a3f827676b5dccef151cb6e2
Soffee <sopee_namyen@hotmail.com>
2021-09-06 17:28:07 +0700

    event detail

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,8 +1,10 @@
 import React from "react";
-import Link from "next/link";
 
 import classes from "./EventItem.module.css";
 import Button from "../ui/button";
+import DateIcon from "../icon/date-icon";
+import AddressIcon from "../icon/address-icon";
+import ArrowRightIcon from "../icon/arrow-right-icon";
 
 const EventItem = ({ title, image, date, location, id }) => {
   const eventDate = new Date(date).toLocaleDateString("en-US", {
@@ -20,15 +22,20 @@ const EventItem = ({ title, image, date, location, id }) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{eventDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
           <Button link={`/events/${id}`}>
-              Explort Event
+            <span>Explort Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
           </Button>
         </div>
       </div>
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
 
 const EventDetailPage = () => {
-    return (
-        <div>
-            <h1>EventDetailPage</h1>
-        </div>
-    )
-}
+  const router = useRouter();
+  const { eventId } = router.query;
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+      return <div>Event not found</div>
+  }
+  
+  return (
+    <div>
+      <h1>EventDetailPage</h1>
+    </div>
+  );
+};
 
 export default EventDetailPage;
